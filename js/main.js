df127ab46d86f19c477035443584b49149b0abad
Isaias S. Silva <luiz.isaias95@gmail.com>
2017-08-28 23:45:41 -0300

    First commit ( ͡° ᴥ ͡°)

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+
+    setTimeout(() => {
+        document.getElementById('loading-img').style = "display:none"
+        document.getElementById('season-table').className = ""
+        console.log('boh ya')
+    },3000)
+
+    //Juntar isso com o codigo de fetch da API 
+    
+}
