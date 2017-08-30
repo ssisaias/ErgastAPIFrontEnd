a98089d6685955f38c1537d66f034bf65be22071
ssisaias <luiz.isaias95@gmail.com>
2017-08-30 18:56:15 -0300

    Show schedule detail implementation start

--- js/main.js
+++ js/main.js
@@ -16,15 +16,12 @@ function fetchSeason (url, httpVerb) {
       document.getElementById('season-table').className = 'visible'
       var seasons = response['MRData']['SeasonTable']['Seasons']
       seasons.reverse()
-      for (var season in seasons) {
-        if (seasons.hasOwnProperty(season)) {
-          var element = seasons[season]
-          document.getElementById('seasons').insertAdjacentHTML('beforeend', `<tr>
-          <td><a onclick="javascript:fetchSchedule('http://ergast.com/api/f1/'+${element.season}+'.json','GET')" class="pure-button" >${element.season}</a></td>
-          <td><a class="pure-button" href="${element.url}" target="_blank"><i class="fa fa-external-link" aria-hidden="true"></i></a></td>
-          </tr>`)
-        }
-      }
+      seasons.forEach(function (element) {
+        document.getElementById('seasons').insertAdjacentHTML('beforeend', `<tr>
+        <td><a onclick="javascript:fetchSchedule('http://ergast.com/api/f1/'+${element.season}+'.json','GET')" class="pure-button" >${element.season}</a></td>
+        <td><a class="pure-button" href="${element.url}" target="_blank"><i class="fa fa-external-link" aria-hidden="true"></i></a></td>
+        </tr>`)
+      }, this)
       // console.log(seasons)
     })
     .catch((err) => {
@@ -34,8 +31,10 @@ function fetchSeason (url, httpVerb) {
 }
 
 function fetchSchedule (url, httpVerb) {
+  document.getElementById('schedule-table').className = 'hidden'
   document.getElementById('schedule-loading').className = 'loader visible'
   document.getElementById('schedules').innerHTML = ''
+  document.getElementById('current-season').innerHTML = ''
   fetch(url, {
     method: httpVerb
   }).then((response) => {
@@ -48,20 +47,27 @@ function fetchSchedule (url, httpVerb) {
       document.getElementById('schedule-loading').className = 'hidden'
       document.getElementById('schedule-table').className = 'visible'
       var races = response['MRData']['RaceTable']['Races']
-      for (var race in races) {
-        if (races.hasOwnProperty(race)) {
-          var element = races[race]
-          document.getElementById('schedules').insertAdjacentHTML('beforeend', `<tr>
-          <td>${element.round}</td>
-          <td>${element.date}</td>
-          <td>${element.time}</td>
-          <td>${element.Circuit.circuitName}</td>
-          <td>${element.Circuit.Location.locality}</td>
-          <td>${element.Circuit.Location.country}</td>
-          <td><a href="${element.url}"><i class="fa fa-external-link" aria-hidden="true"></i></a></td>
-          </tr>`)
-        }
-      }
+      races.forEach(function (element) {
+        document.getElementById('schedules').insertAdjacentHTML('beforeend', `<tr>
+        <td>${element.round}</td>
+        <td>${element.date}</td>
+        <td>${element.time}</td>
+        <td>${element.Circuit.circuitName}</td>
+        <td>${element.Circuit.Location.locality}</td>
+        <td>${element.Circuit.Location.country}</td>
+        <td><a href="${element.url}" target="_blank"><i class="fa fa-external-link" aria-hidden="true"></i></a></td>
+        </tr>`)
+      }, this)
+      document.getElementById('current-season').innerHTML = `Temporada ${response['MRData']['RaceTable']['season']}`
+      document.getElementById('current-season-winner').innerHTML = `Campeão da temporada: ${response['MRData']['RaceTable']['season']}`
+      console.log(`http://ergast.com/api/f1/${response['MRData']['RaceTable']['season']}/driverStandings/1.json`)
+      // Erro aqui!
+      fetch(`http://ergast.com/api/f1/${response['MRData']['RaceTable']['season']}/driverStandings/1.json`).then((response)=>{
+        var json = response.json()
+        var champs = json['MRData']['StandingsTable']['StandingsLists']['DriverStandings']['Driver']
+        var name = champs.givenName + ' ' + champs.familyName
+        document.getElementById('current-season-winner').innerHTML = `Campeão da temporada: ${name}`
+      })
       // console.log(seasons)
     })
     .catch((err) => {
