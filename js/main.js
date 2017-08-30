e2a56d9245b6767748c9d12b681b11eb9f960924
ssisaias <luiz.isaias95@gmail.com>
2017-08-30 15:08:36 -0300

    Fetch season data from api and show on browser

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
-function btnPress () {
-  document.getElementById('loading-img').style = 'display:none'
+/*  var season = fetchFromAPI('http://ergast.com/api/f1/seasons.json?limit=1&offset=67', 'GET')
+  if (season) {
+    document.getElementById('loading-img').style = 'display:none'
+  }
+*/
+window.onload = () => {
+  fetchSeason('http://ergast.com/api/f1/seasons.json?limit=68', 'GET')
+}
 
-  fetch('http://ergast.com/api/f1/seasons.json?limit=1&offset=67',{method: "GET"}).then
-  ( (response)=> {
-    if(response.ok){
-      return response.json()
-    }
-  }).then((response)=>{
-    alert(response["MRData"]["SeasonTable"]["Seasons"][0]["season"])
-    console.log(response)
-  })
-  .catch((err)=>{
-    console.log('Deu ruim ' + err.message)
-  })
-
+function fetchSeason (url, httpVerb) {
+  fetch(url, {
+    method: httpVerb
+  }).then((response) => {
+      if (response.ok) {
+        return response.json()
+      }
+    })
+    .then((response) => {
+      document.getElementById('season-loading').className = 'hidden'
+      document.getElementById('season-table').className = 'visible'
+      var seasons = response['MRData']['SeasonTable']['Seasons']
+      for (var season in seasons) {
+        if (seasons.hasOwnProperty(season)) {
+          var element = seasons[season]
+          document.getElementById('seasons').insertAdjacentHTML('beforeend',`<tr>
+          <td><a href="${element.url}">${element.season}</a></td>
+          </tr>`)
+        }
+      }
+      // console.log(seasons)
+    })
+    .catch((err) => {
+      console.log(err.message)
+      return undefined
+    })
 }
