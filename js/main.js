aa7fea9eb3ccc54f869bad53da8f50dfe6115f6f
Isaias S. Silva <luiz.isaias95@gmail.com>
2017-08-30 00:16:27 -0300

    function fetch API code

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 function btnPress () {
   document.getElementById('loading-img').style = 'display:none'
 
-  loadSeason().then((result) => {
-    var Season = JSON.parse(result)
-
-    console.log(Season)
-    // console.log(Season)
-  }, (err) => {
-    console.log(err)
+  fetch('http://ergast.com/api/f1/seasons.json?limit=1&offset=67',{method: "GET"}).then
+  ( (response)=> {
+    if(response.ok){
+      return response.json()
+    }
+  }).then((response)=>{
+    alert(response["MRData"]["SeasonTable"]["Seasons"][0]["season"])
+    console.log(response)
+  })
+  .catch((err)=>{
+    console.log('Deu ruim ' + err.message)
   })
 
-  console.log('boh ya')
 }
